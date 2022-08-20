b163ff94a2a18763901c8b044615f9f9fd56f8b6
Max Romanov <max.romanov@gmail.com>
2022-08-20 09:02:13 +0300

    Code size optimization.
    Replacing case with table.

--- firmware/src/midi_cmd.h
+++ firmware/src/midi_cmd.h
@@ -79,12 +79,8 @@ struct midi_cmd_t {
     }
 
     static uint8_t command(uint8_t b) {
-        if (is_midi_cmd(b)) {
-            uint8_t c = b & CMD_SYS;
-            return c != CMD_SYS ? c : b;
-        }
-
-        return 0;
+        return is_midi_cmd(b)
+            * ((b & 0xF0) + is_midi_sys(b) * (b & 0x0F));
     }
 
     bool sys_ex() const { return sys_ex_ || (size_ > 0 && command() == CMD_SYS_EX); }
@@ -101,35 +97,38 @@ struct midi_cmd_t {
     uint8_t size() const { return size_; }
 
     static uint8_t cmd_size(uint8_t b) {
-        switch (command(b)) {
-        case CMD_NOTE_OFF:
-        case CMD_NOTE_ON:
-        case CMD_KEY_PRESSURE:
-        case CMD_CTRL_CHANGE:
-        case CMD_PITCH_CHANGE:
-        case CMD_SYS_SONG_PP:
-            return 3;
+        // ((b & 0x70) >> 4) + ((b & CMD_SYS) == CMD_SYS) * (b & 0x0F)
+        static uint8_t cmd2size[] = {
+            0,
+            3, // CMD_NOTE_OFF      = 0x80U,
+            3, // CMD_NOTE_ON       = 0x90U,
+            3, // CMD_KEY_PRESSURE  = 0xA0U,
+            3, // CMD_CTRL_CHANGE   = 0xB0U,
+            2, // CMD_PROG_CHANGE   = 0xC0U,
+            2, // CMD_CHAN_PRESSURE = 0xD0U,
+            3, // CMD_PITCH_CHANGE  = 0xE0U,
+            2, // CMD_SYS_EX        = 0xF0U,
+            2, // CMD_SYS_MTC       = 0xF1U,
+            3, // CMD_SYS_SONG_PP   = 0xF2U,
+            2, // CMD_SYS_SONG_SEL  = 0xF3U,
+            0, // ? = 0xF4U,
+            0, // ? = 0xF5U,
+            1, // CMD_SYS_TUNE_REQ  = 0xF6U,
+            1, // CMD_SYS_EX_END    = 0xF7U,
+            1, // CMD_SYS_CLOCK     = 0xF8U,
+            1, // CMD_SYS_TICK      = 0xF9U,
+            1, // CMD_SYS_START     = 0xFAU,
+            1, // CMD_SYS_CONT      = 0xFBU,
+            1, // CMD_SYS_STOP      = 0xFCU,
+            1, // CMD_SYS_UNDEF     = 0xFDU,
+            1, // CMD_SYS_ACTIVE_S  = 0xFEU,
+            1, // CMD_SYS_RESET     = 0xFFU,
+        };
 
-        case CMD_PROG_CHANGE:
-        case CMD_CHAN_PRESSURE:
-        case CMD_SYS_MTC:
-        case CMD_SYS_SONG_SEL:
-        case CMD_SYS_EX:
-            return 2;
+        uint8_t n = is_midi_cmd(b)
+            * (1 + ((b & 0x70) >> 4) + is_midi_sys(b) * (b & 0x0F));
 
-        case CMD_SYS_EX_END:
-        case CMD_SYS_CLOCK:
-        case CMD_SYS_TICK:
-        case CMD_SYS_START:
-        case CMD_SYS_CONT:
-        case CMD_SYS_STOP:
-        case CMD_SYS_UNDEF:
-        case CMD_SYS_ACTIVE_S:
-        case CMD_SYS_RESET:
-            return 1;
-        }
-
-        return 0;
+        return cmd2size[n];
     }
 
     bool ready() const {
