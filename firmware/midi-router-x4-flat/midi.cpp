d5b530a8a14d26299dd75eb621e0f9392e994df9
Maxim Romanov <mromanov@elvees.com>
2022-11-23 20:38:53 +0300

    Start working on Note->CC convertor

--- firmware/midi-router-x4-flat/midi.cpp
+++ firmware/midi-router-x4-flat/midi.cpp
@@ -2,6 +2,7 @@
 #include <midi.h>
 #include <uart.h>
 #include <crit_sec.h>
+#include <splitter.h>
 #include <merger.h>
 
 namespace {
@@ -23,18 +24,135 @@ template<> uart_e0::tx_ring_t uart_e0::tx_ring = {};
 template<> uint8_t uart_e0::want_write = 0;
 
 using ALL = uart_list<uart_c0, uart_c1, uart_d0, uart_e0>;
+using MERGE_SRC = uart_list<uart_d0, uart_e0, uart_c0>;
+using MERGE_DST = uart_list<uart_c0>;
+using SPLIT_SRC = uart_c1;
+using SPLIT_DST = uart_list<uart_c1, uart_d0, uart_e0>;
 
-midi::merger_t<ALL, ALL> merger_state;
-template<> midi::merger_state_t midi::merger_t<ALL, ALL>::state = { };
+midi::merger_t<MERGE_SRC, MERGE_DST> merger_state;
+template<> midi::merger_state_t midi::merger_t<MERGE_SRC, MERGE_DST>::state = { };
+
+midi::splitter_t<SPLIT_SRC, SPLIT_DST> splitter_state;
+template<> midi::splitter_state_t midi::splitter_t<SPLIT_SRC, SPLIT_DST>::state = { };
+
+enum {
+    WAIT_CC,
+    WAIT_0x14,
+    WAIT_DATA
+} port2_state = WAIT_CC;
+
+enum {
+    WAIT_NOTE_ON,
+    WAIT_TONE,
+    WAIT_VELOCITY
+} port3_state = WAIT_NOTE_ON;
+
+enum {
+    NOTE_START = NOTE_C2, // C2
+    NOTE_END = NOTE_Cs4,  // C#4
+
+    FALLBACK_CC = 12,
+
+    NOTE_VEL_DEC = 10,
+};
+
+uint8_t note2ccs_map[] = {
+    /* C2  */  20, // 21, 22, 23
+    /* C#2 */  24, // 25, 26, 27
+    /* D2  */  28, // 29, 30, 31
+
+    /* E3  */ 102, // 103, 104
+    /* F3  */ 105, // 106, 107
+    /* F#3 */ 108, // 109
+};
+
+uint8_t note2cc_map[] = {
+    /* C2  */  20, // 21, 22, 23
+    /* C#2 */  24, // 25, 26, 27
+    /* D2  */  28, // 29, 30, 31
+    /* D#2 */  52,
+    /* E2  */  53,
+    /* F2  */  54,
+    /* F#2 */  61,
+    /* G2  */  55,
+    /* G#2 */  56,
+    /* A2  */  57,
+    /* A#2 */  58,
+    /* B2  */  13,
+    /* C3  */  75,
+    /* C#3 */  76,
+    /* D3  */  77,
+    /* D#3 */  78,
+    /* E3  */ 102, // 103, 104
+    /* F3  */ 105, // 106, 107
+    /* F#3 */ 108, // 109
+    /* G3  */  62,
+    /* G#3 */  40,
+    /* A3  */  41,
+    /* A#3 */  42,
+    /* B3  */  43,
+    /* C4  */  59,
+};
 
 void merger_rx_complete(uint8_t port, uint8_t data, bool ferr) {
     midi::mon(port, true, &data, 1);
 
-    merger_state.rx_complete(port, data, ferr);
+    if (port == 3) {
+        if (!is_midi_rt(data)) {
+            switch (port3_state) {
+            case WAIT_NOTE_ON:
+                if (midi_cmd_t::command(data) == CMD_NOTE_ON) {
+                    port3_state = WAIT_TONE;
+                    data = CMD_CTRL_CHANGE | (data & 0x0FU);
+                }
+                break;
+            case WAIT_TONE:
+                port3_state = WAIT_VELOCITY;
+                if (data >= NOTE_START && data < NOTE_END) {
+                    data = note2cc_map[data - NOTE_START];
+                } else {
+                    data = FALLBACK_CC;
+                }
+                break;
+            case WAIT_VELOCITY:
+                port3_state = WAIT_NOTE_ON;
+                data = (data < NOTE_VEL_DEC) ? 0 : data - NOTE_VEL_DEC;
+                break;
+            }
+        }
+
+        splitter_state.rx_complete(port, data, ferr);
+    } else {
+        if (port == 2) {
+            switch (port2_state) {
+            case WAIT_CC:
+                if (data == CMD_CTRL_CHANGE) {
+                    port2_state = WAIT_0x14;
+                }
+                break;
+            case WAIT_0x14:
+                if (data == 0x14) {
+                    port2_state = WAIT_DATA;
+                } else {
+                    port2_state = WAIT_CC;
+                }
+                break;
+            case WAIT_DATA:
+                port2_state = WAIT_CC;
+                break;
+            }
+        }
+
+        merger_state.rx_complete(port, data, ferr);
+    }
 }
 
 void merger_process_dre(uint8_t port) {
-    merger_state.process_dre(port);
+    if (port == 3) {
+        splitter_state.process_dre(port);
+    } else {
+        merger_state.process_dre(port);
+    }
 }
 
 template<typename T>
@@ -72,11 +190,13 @@ void init(process_byte_t cb) {
 
     if (cb) {
         merger_state.disable();
+        splitter_state.disable();
 
         on_rx_complete = cb;
         on_dre = dummy_process_dre;
     } else {
         merger_state.enable();
+        splitter_state.enable();
 
         on_rx_complete = merger_rx_complete;
         on_dre = merger_process_dre;
@@ -87,9 +207,11 @@ uint8_t send(uint8_t port, const uint8_t *buf, uint8_t size) {
     return ALL::write_buf(port, buf, size);
 }
 
-void pending_timeout() {
-    crit_sec cs;
-    merger_state.pending_timeout();
+void timer_update(unsigned long t) {
+    if (pending_timer.update(t)) {
+        crit_sec cs;
+        merger_state.pending_timeout();
+    }
 }
 
 void dump_state() {
